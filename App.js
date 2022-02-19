54aa877f5bbe0529edace737f8c5f9e9d6e41456
jimbo23 <kiefersoon@gmail.com>
2022-02-19 18:15:22 +0800

    initialize app, set up basic navigation

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="wow" onPress={() => navigation.navigate("detail")} />
     </View>
   );
-}
+};
+
+const DetailScreen = () => (
+  <View style={styles.container}>
+    <Text>Detail</Text>
+  </View>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="home" component={HomeScreen} />
+      <Stack.Screen name="detail" component={DetailScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
